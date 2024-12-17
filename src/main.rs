0b3860b50f88269d537bbaf8619b933d3c3440ea
shir <shirans@eyenet-mobile.com>
2024-12-17 11:01:14 +0200

    adding logs to the post endpoint including a request id

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::net::TcpListener;
+use env_logger::Env;
 use zero2prod::{configuration::get_configuration, startup::run};
 use zero2prod::in_memory::AppState;
 use actix_web::web;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // create the in-memory list for sharing accross threads
     let app_state: AppState = AppState::new();
     let data_store_shared = web::Data::new(app_state);
+    // creating the properties data from our configuration.yaml file
     let configuration = get_configuration().expect("Failed to read configuration");
+    // building the servers adress and port
     let address = format!("127.0.0.1:{}", configuration.server_port);
+    // binding to port
     let listener = TcpListener::bind(address).expect("Binding to port failed");
-    // Arrange
+    // running the receiving http server
     run(listener, data_store_shared)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::in_memory::{AppState, Subscription};
 
@@ -21,14 +22,21 @@ impl SubscriptionRequest {
 
 
 pub async fn subscribe(info: web::Json<SubscriptionRequest>, app_state: web::Data<AppState>) -> HttpResponse {
-    println!("{}",info.username);
-    println!("{}",info.email);
+    let request_id = Uuid::new_v4();
     if info.username == ""{
+        log::warn!("request_id {} - request for email {} was missing username", request_id, info.email);
         return HttpResponse::BadRequest().body("username is missing")
     }
     if info.email == ""{
+        log::warn!("request_id {} - request for username {} was missing email", request_id, info.username);
         return HttpResponse::BadRequest().body("email is missing")
     }
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        info.email,
+        info.username
+        );
 
     let mut subscriptions = app_state.subscriptions.write().expect("RwLock poisoned");
     let id = app_state.get_id();
@@ -38,7 +46,16 @@ pub async fn subscribe(info: web::Json<SubscriptionRequest>, app_state: web::Dat
         email: info.email.to_string(),
     };
 
+
+
     subscriptions.push(subscription);
+    log::info!(
+        "request_id {} - Added '{}' '{}' as a new subscriber successfully with id {}.",
+        request_id,
+        info.email,
+        info.username,
+        id
+        );
 
     HttpResponse::Ok().json(id)
     //HttpResponse::Ok().finish()
